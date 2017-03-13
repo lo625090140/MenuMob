bd02a13f0393879ce52bc61b3721216a807bb192
chenjt <625090140@qq.com>
2017-03-13 14:50:35 +0800

    Upload

--- app/src/main/java/com/demo/mob/fragment/HttpFragment.java
+++ app/src/main/java/com/demo/mob/fragment/HttpFragment.java
@@ -21,6 +21,7 @@ import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.FormBody;
 import okhttp3.MediaType;
+import okhttp3.MultipartBody;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.RequestBody;
@@ -34,7 +35,7 @@ public class HttpFragment extends BaseFragment{
     private OkHttpClient okHttpClient = new OkHttpClient();
     private TextView result;
     //http://localhost:8185/Test/login?username=haha&password=123
-    private String baseurl = "http://192.168.44.128:8185/Test/";
+    private String baseurl = "http://192.168.44.191:8185/Test/";
 
     @Override
     protected int getLayoutId() {
@@ -47,9 +48,12 @@ public class HttpFragment extends BaseFragment{
         view.findViewById(R.id.okhttp_PostString).setOnClickListener(this);
         view.findViewById(R.id.okhttp_Post).setOnClickListener(this);
         view.findViewById(R.id.okhttp_PostFile).setOnClickListener(this);
+        view.findViewById(R.id.okhttp_Upload).setOnClickListener(this);
         result = (TextView) view.findViewById(R.id.okhttp_Text);
     }
 
+
+    //get请求
     private void doGet(){
         //1.拿到httpClient对象
         //OkHttpClient okHttpClient = new OkHttpClient(); 提升为全局不至于每次都new
@@ -60,6 +64,7 @@ public class HttpFragment extends BaseFragment{
         executeRequest(request);
     }
 
+    //post请求
     private void doPost() {
         //2.构造Request和RequestBody
         RequestBody body = new FormBody.Builder()
@@ -72,6 +77,8 @@ public class HttpFragment extends BaseFragment{
         executeRequest(request);
     }
 
+
+    //提交字符串
     private void doPostString() {
         //2.构造Request和RequestBody
         RequestBody body = RequestBody.create(MediaType.parse("text/plain;chaset=utf-8"),"{username:chenjt,password:123456}");
@@ -81,6 +88,8 @@ public class HttpFragment extends BaseFragment{
         executeRequest(request);
     }
 
+
+    //提交文件
     private void doPostFile() {
         File file = new File(App.PATH);
         if (!file.exists()){
@@ -94,6 +103,26 @@ public class HttpFragment extends BaseFragment{
         Request request = builder.get().url(baseurl + "postFile").post(body).build();
         executeRequest(request);
     }
+
+    //上传文件
+    private void doUpload(){
+        File file = new File(App.PATH);
+        if (!file.exists()){
+            Logs.e(Tag,"图片不存在");
+            return;
+        }
+        //百度搜索MediaType表
+        MultipartBody body = new MultipartBody.Builder()
+                .setType(MultipartBody.FORM)
+                .addFormDataPart("username","chenjt")
+                .addFormDataPart("password","123456")
+                .addFormDataPart("photo","tp.jpg",RequestBody.create(MediaType.parse("application/octet-stream"),file))
+                .build();
+        Request.Builder builder = new Request.Builder();
+        Request request = builder.get().url(baseurl + "uploadInfo").post(body).build();
+        executeRequest(request);
+    }
+
 
     private void executeRequest(Request request) {
         //3.将Request封装成Call
@@ -151,8 +180,10 @@ public class HttpFragment extends BaseFragment{
                 doPostString();
                 break;
             case R.id.okhttp_PostFile:
-
                 doPostFile();
+                break;
+            case R.id.okhttp_Upload:
+                doUpload();
                 break;
         }
     }
