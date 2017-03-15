25b4e0b95c4c6f7859d704a3639c1d5b792168b3
chenjt <625090140@qq.com>
2017-03-15 13:58:00 +0800

    update

---
+++ app/src/main/java/com/demo/mob/fragment/MobLinkFragment.java
@@ -0,0 +1,145 @@
+package com.demo.mob.fragment;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.os.Message;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.demo.mob.activity.R;
+import com.demo.mob.utils.BaseFragment;
+import com.demo.mob.utils.ToastUtil;
+import com.mob.moblink.ActionListener;
+import com.mob.moblink.MobLink;
+import com.mob.tools.utils.ResHelper;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.util.HashMap;
+
+import cn.sharesdk.framework.Platform;
+import cn.sharesdk.framework.ShareSDK;
+import cn.sharesdk.wechat.friends.Wechat;
+
+/**
+ * Created by chenjt on 2017/3/15.
+ */
+
+public class  MobLinkFragment extends BaseFragment{
+    public static final String[] MAIN_PATH_ARR = {"/demo/a", "/demo/b", "/demo/c"};
+    public static final String IP = "192.168.44.191:8185";
+    public static final String SHARE_URL = "http://" + IP + "/Test";//"http://f.moblink.mob.com";
+    public static final String APPKEY = "1c12352b1abd0";//"1b8898cb51ccb";//正式部署到测试环境 //测试 d9b58a07cbf4
+    private String mobID;
+    private TextView tv;
+    private int selectedID;
+    @Override
+    protected int getLayoutId() {
+        return R.layout.fragment_moblink;
+    }
+
+    @Override
+    protected void initContentView(Bundle savedInstanceState) {
+        init();
+    }
+
+    private void init() {
+        ShareSDK.initSDK(context);
+        MobLink.initSDK(context, APPKEY);
+        view.findViewById(R.id.mobid).setOnClickListener(this);
+        view.findViewById(R.id.share).setOnClickListener(this);
+        (tv = (TextView) view.findViewById(R.id.text)).setOnClickListener(this);
+    }
+
+    @Override
+    public boolean handleMessage(Message msg) {
+        return false;
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.mobid:
+                HashMap<String, Object> params = new HashMap<String, Object>();
+                String source = "mob";
+                MobLink.getMobID(null, MAIN_PATH_ARR[selectedID], source, new ActionListener() {
+                    public void onResult(HashMap<String, Object> params) {
+                        if (params != null && params.containsKey("mobID")) {
+                            mobID = String.valueOf(params.get("mobID"));
+                            Log.e("Tag_onResult", mobID);
+                            ToastUtil.show(context,mobID);
+                        } else {
+                            ToastUtil.show(context,"Get MobID Failed!");
+                        }
+                    }
+
+                    public void onError(Throwable t) {
+                        Log.e("Tag_onError", t != null ?t.getMessage() : "空的");
+                        if (t != null) {
+                            ToastUtil.show(context,t.getMessage());
+                        }
+                    }
+                });
+                break;
+            case R.id.share:
+                String shareUrl = SHARE_URL + MAIN_PATH_ARR[selectedID] + ".html";
+                if (!TextUtils.isEmpty(mobID)) {
+                    shareUrl += "?mobid=" + mobID;
+                    // "http://f.moblink.mob.com/demo/a/?mobid="+mobID
+                }
+                String title = "我是测试的Title";
+                String text = "我是测试的Text";
+                String imgPath = copyImgToSD(context, R.mipmap.ic_launcher , "moblink");
+                Platform.ShareParams sp = new Platform.ShareParams();
+                sp.setTitle(title);;
+                sp.setText(text);
+                sp.setImagePath(imgPath);
+                sp.setUrl(shareUrl);
+                sp.setShareType(Platform.SHARE_WEBPAGE);
+                Platform plat = ShareSDK.getPlatform(Wechat.NAME);
+                plat.share(sp);
+                break;
+            case R.id.text:
+                if (tv.getText().toString().trim().equals("A")){
+                    tv.setText("B");
+                    selectedID = 1;
+                }else if (tv.getText().toString().trim().equals("B")){
+                    tv.setText("C");
+                    selectedID = 2;
+                }else if (tv.getText().toString().trim().equals("C")){
+                    tv.setText("A");
+                    selectedID = 0;
+                }
+                break;
+        }
+    }
+
+    public static String copyImgToSD(Context context, int imgID, String imgName) {
+        String imgPaht = "";
+        Bitmap bitmap = BitmapFactory.decodeResource(context.getResources(), imgID);
+        if (bitmap != null && !bitmap.isRecycled()) {
+            String path = ResHelper.getImageCachePath(context);
+            if (TextUtils.isEmpty(imgName)) {
+                imgName = String.valueOf(System.currentTimeMillis());
+            }
+            File file = new File(path, imgName + ".jpg");
+            if (file.exists()) {
+                return file.getAbsolutePath();
+            }
+            try {
+                FileOutputStream fos = new FileOutputStream(file);
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 80, fos);
+                fos.flush();
+                fos.close();
+                imgPaht = file.getAbsolutePath();
+            } catch (Throwable t) {
+            }
+        }
+        return imgPaht;
+    }
+}
---
+++ app/src/main/java/com/demo/mob/link_activity/Activity_A.java
@@ -0,0 +1,78 @@
+package com.demo.mob.link_activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Message;
+import android.util.Log;
+import android.view.View;
+
+import com.demo.mob.activity.R;
+import com.demo.mob.utils.BaseActivity;
+import com.mob.moblink.ActionListener;
+import com.mob.moblink.MobLink;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Activity_A extends BaseActivity {
+    private String path = "/demo/a";
+    public static final String APPKEY = "1c12352b1abd0";//正式部署到测试环境 //测试 d9b58a07cbf4
+    private String source;
+    private String paramStr = "";
+    @Override
+    public boolean handleMessage(Message msg) {
+        return false;
+    }
+
+    @Override
+    protected void initContentView(Bundle savedInstanceState) {
+        setContentView(R.layout.activitya);
+        init();
+    }
+
+    private void init() {
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
+    }
+    protected void onResume() {
+        super.onResume();
+        //初始化moblink SDK
+        MobLink.initSDK(this, APPKEY);
+        //设置场景还原监听
+        MobLink.setIntentHandler(getIntent(), new ActionListener() {
+            public void onResult(final HashMap<String, Object> res) {
+                runOnUiThread(new Runnable() {
+                    public void run() {
+                        if (res != null) {
+                            if (res.get("source") != null) {
+                                source = String.valueOf(res.get("source"));
+                            }
+                            if (res.get("params") != null) {
+                                HashMap<String, Object> params = (HashMap<String, Object>) res.get("params");
+                                for (Map.Entry<String, Object> entry : params.entrySet()) {
+                                    paramStr += (entry.getKey() + " : " + entry.getValue() + "\r\n");
+                                }
+                            }
+                        }
+
+
+                    }
+                });
+            }
+            public void onError(Throwable t) {
+                if (t != null) {
+                    Log.e(Tag, t.getMessage());
+                }
+            }
+        });
+
+    }
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        setIntent(intent);
+    }
+}
